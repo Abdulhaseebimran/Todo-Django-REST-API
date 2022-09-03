ebbcf6bb50b2dd210ac836df8f2615a30136bd40
Abdulhaseebimran <haseebimran0001@gamil.com>
2022-09-03 16:02:51 +0500

    todo rest api django & Heroku Deployment

---
+++ todo_api/urls.py
@@ -0,0 +1,11 @@
+# from django.conf.urls import url
+from django.urls import path
+from .views import (
+    TodoListApiView,
+    TodoDetailApiView
+)
+
+urlpatterns = [
+    path('api/', TodoListApiView.as_view()),
+    path('api/<int:todo_id>/', TodoDetailApiView.as_view()),
+]
